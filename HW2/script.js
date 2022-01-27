31ee05e4802a3f476be7ad12aa36872a3a9b9d29
Becky <77357440+BeckyGH@users.noreply.github.com>
2022-01-27 15:16:32 -0700

    week two assignment

---
+++ HW2/script.js
@@ -0,0 +1,21 @@
+
+
+console.log("three board games I love:(because I'm not much into computer games)");
+console.log("trouble, clue, and scrabble");
+//i'm not sure if this is correct????
+console.log("three of my fav website are:");
+console.log("https://meowwolf.com, https://www.nationalgeographic.com, https://www.badmanners.com/ ");
+console.log(`three of my favorite artists & their cultural importance: I picked historical artist/painters, these three have always been ones I admired
+for their ability to break the "rules" during their times and be authentic in their exploration and creativity.`);
+//FRIDA KAHLO--------------------why was she culturally important?
+console.log(`Frida Kahlo-She was most influential because she broke numerous barriers, for latina women and all women, in painting  at the time. She wasn't 
+afraid of expressing her truth on a canvas.`);
+//Edvard Munch-------------------why was he culturally important?
+console.log(`Edvard Munch-He was all about breaking the (then) rules of painting what is seen to what is felt. He was willing and wanted to explore deeper within himself, 
+using his paintings as relfections of expressive emotions.`);
+//Wassily Kadinsky---------------why was he culturally important?-
+console.log(`Wassily Kadinsky-Kandisky was coined as a more "spiritual painter", which wasn't well regarded or usual during his time. He combined his knowledge of psychology 
+and color symbolism to create expressive colorful painting of shape and form that hadn't really been explored before. Definetly an influential artists in 
+helping to create the world of abstract art.`)
+
+
