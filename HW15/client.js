d8221b63aa2343f046fd3a350ed02b87b07330b0
Rebecca Gallegos <77357440+BeckyGH@users.noreply.github.com>
2022-05-04 12:45:02 -0600

    Update client.js

--- HW15/client.js
+++ HW15/client.js
@@ -1,39 +1,64 @@
+onst http = require('http');
+var fs = require('fs');
+const querystring = require("querystring");
 
-const http = require('http');
 // Options to be used by request 
 let options =
 {
   host: 'localhost',
   port: '5501',
   path: 'index.html',
-  method: 'GET',
-  headers: {
-    'Accept': 'application/json',
-    'firstName': '',
-    'lastName': ''
-  }
-
+  method: 'GET' 
 };
 
-// Callback function is used to deal with response
-let callback = function (response) {
-  // Continuously update stream with data
-  var body = '';
-  response.on('data', function (data) {
-    body += data;
-    //querystring.parse('firstName=&lastName=&norefresh=1','&','=');
-    //querystring.stringify({ firstName: '', lastName: '' }, ';', ':');
-  });
+// Callback function is used to deal with response------
+let callback = function (response)
+ {
+      // Continuously update stream with data
+      var body = '';
+      response.on('data', function (data) 
+      {
+            body+= data;
+            console.log(obj);
+      });
+
+
+//this where I would put the readerStream---- to do the file stuff?????
 
   response.on('end', function () {
-    // Data received completely.
-    console.log(body);
-
+                
+    var stream = fs.createReadStream('index.html',
+    {
+      
+      flags: 'r',
+      encoding: 'utf8',
+      method: 'GET'
+      headers: 
+      {
+         form: '',
+          id:''
+          value:''
+      }
+    });
+    
+    var writerStream = fs.createWriteStream('output.txt',
+    {
+      flags: 'w',
+      encoding: 'utf8',
+  
+    });
+    // Pipe the read and write operations
+    stream.pipe(writerStream);
+   
+    //console.log(JSON.parse(body));
+    console.log("piping Ended");
   });
 }
 // Make a request to the server
 let req = http.request(options, callback);
 req.end();
+
+  
 
   ////>>>>>>>>>>>>>others i looked into but did not work
 
