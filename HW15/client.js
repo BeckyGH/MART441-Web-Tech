387f59dd1852e9aa4be61b4f72edeed26af5a56f
Rebecca Gallegos <77357440+BeckyGH@users.noreply.github.com>
2022-05-04 13:11:35 -0600

    Update client.js

--- HW15/client.js
+++ HW15/client.js
@@ -1,4 +1,5 @@
-onst http = require('http');
+
+const http = require('http');
 var fs = require('fs');
 const querystring = require("querystring");
 
@@ -11,99 +12,49 @@ let options =
   method: 'GET' 
 };
 
-// Callback function is used to deal with response------
+// Callback function is used to deal with response
 let callback = function (response)
  {
-      // Continuously update stream with data
-      var body = '';
-      response.on('data', function (data) 
-      {
-            body+= data;
-            console.log(obj);
-      });
-
-
-//this where I would put the readerStream---- to do the file stuff?????
-
-  response.on('end', function () 
-  {
-                
-      var stream = fs.createReadStream('index.html',
-      {
-
-        flags: 'r',
-        encoding: 'utf8',
-        method: 'GET'
-        headers: 
+        // Continuously update stream with data
+        var body = '';
+        response.on('data', function (data) 
         {
-           form: '',
-            id:''
-            value:''
-        }
-      });
+              body+= data;
+              console.log(obj);
+        });
 
-      var writerStream = fs.createWriteStream('output.txt',
-      {
-        flags: 'w',
-        encoding: 'utf8',
 
-      });
+//this where I would put the readerStream---- to do the file stuff
+
+        response.on('end', function () 
+        {
+
+            var stream = fs.createReadStream('index.html',
+            {
+
+              flags: 'r',
+              encoding: 'utf8',
+
+
+            });
+            var writerStream = fs.createWriteStream('output.txt',
+            {
+              flags: 'w',
+              encoding: 'utf8',
+
+              headers: {
+                 value: ''
+              }
+            });
       // Pipe the read and write operations
       stream.pipe(writerStream);
-
+   
       //console.log(JSON.parse(body));
       console.log("piping Ended");
-    });
- }
+  });
+}
 // Make a request to the server
 let req = http.request(options, callback);
 req.end();
 
   
-
-  ////>>>>>>>>>>>>>others i looked into but did not work
-
-/* // Continuously update stream with data
-  const req = http.request(options, (res) => {
-     let data = ''
-      
-     res.on('data', (chunk) => {
-         data += chunk;
-     });
-     
-     // Ending the response 
-     res.on('end', () => {
-         console.log('Body:', JSON.parse(data))
-     });
-        
- }).on("error", (err) => {
-     console.log("Error: ", err)
- }).end()
-*/
-/*const http = require('http');
-
-let request = http.get('http://127.0.0.1:5502/index.html', (res) => {
-  if (res.statusCode !== 200) {
-    console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
-    res.resume();
-    return;
-  }
-
-  let data = '';
-
-  res.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  res.on('close', () => {
-    console.log('Retrieved all data');
-    console.log(JSON.parse(data));
-  });
-});
-
-request.on('error', (err) => {
-  console.error(`Encountered an error trying to make a request: ${err.message}`);
-});
-*/
-
-
